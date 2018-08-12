f14727d4f6edbe6b312533bb1529cff3081ff2b4
ENGSamShamsan <41812691+ENGSamShamsan@users.noreply.github.com>
2018-08-11 20:26:38 -0700

    Create Web_Scrapping.py
    This is a working code

---
+++ Web_Scrapping.py
@@ -0,0 +1,28 @@
+# Web_Scrapping
+
+# Sam Shamsan, Saturday, Aug 11, 2018
+# Platfrom imported : requests, lxml and BeautifulSoup + html5lib
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+'''This is a quick WORKING snipit for Web Scraping
+    It featch the html page content and print it
+    '''
+# Get the linke to the webpage you want to scrap
+http = requests.get("https://abc7news.com/")
+# Soup out the content using lxml schema and beautifulSoup library
+soup = BeautifulSoup(http.content, 'lxml')
+# You can print the soup now
+#print(soup)
+# Find out the content attirbute inside the page
+main_news= soup.find('meta', itemprop='name')['content']
+# You can print the result of you search
+#print(main_news)
+# Now you can parse
+main_news= main_news.lower().split()
+
+print(type(main_news))
+print(main_news)
+
+#meta itemprop="name"
